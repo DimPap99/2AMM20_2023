e75c689f6a0d5e5af986bfadec0f46ba848ffaa2
DimPap99 <dimtrispap1999@gmail.com>
2023-10-14 16:49:39 +0200

    last code changes

--- helpers.py
+++ helpers.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import csv
 import pickle
-
+import warnings
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+import statsmodels.api as sm
+from scipy.stats._continuous_distns import _distn_names
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
 def pickle_data(file_path, obj, verbose=False):
     try:
         with open(file_path, 'wb') as f:
@@ -109,3 +117,90 @@ def bfs(graph, start, keep_direction=False):
                         if neighbor not in visited:
                             queue.append(neighbor)
             return pairs, visited
+
+
+
+# Create models from data
+def best_fit_distribution(data, bins=100, ax=None, verbose=False):
+    """Model data by finding best fit distribution to data"""
+    # Get histogram of original data
+    y, x = np.histogram(data, bins=bins, density=True)
+    x = (x + np.roll(x, -1))[:-1] / 2.0
+
+    # Best holders
+    best_distributions = []
+
+    # Estimate distribution parameters from data
+    for ii, distribution in enumerate([d for d in _distn_names if not d in ['levy_stable', 'studentized_range']]):
+        if verbose:
+            print("{:>3} / {:<3}: {}".format( ii+1, len(_distn_names), distribution ))
+
+        distribution = getattr(st, distribution)
+
+        # Try to fit the distribution
+        try:
+            # Ignore warnings from data that can't be fit
+            with warnings.catch_warnings():
+                warnings.filterwarnings('ignore')
+                
+                # fit dist to data
+                params = distribution.fit(data)
+
+                # Separate parts of parameters
+                arg = params[:-2]
+                loc = params[-2]
+                scale = params[-1]
+                
+                # Calculate fitted PDF and error with fit in distribution
+                pdf = distribution.pdf(x, loc=loc, scale=scale, *arg)
+                sse = np.sum(np.power(y - pdf, 2.0))
+                
+                # if axis pass in add to plot
+                try:
+                    if ax:
+                        pd.Series(pdf, x).plot(ax=ax)
+                    
+                except Exception:
+                    pass
+
+                # identify if this distribution is better
+                best_distributions.append((distribution, params, sse))
+        
+        except Exception:
+            pass
+
+    
+    return sorted(best_distributions, key=lambda x:x[2])
+
+def make_pdf(dist, params, size=10000):
+    """Generate distributions's Probability Distribution Function """
+
+    # Separate parts of parameters
+    arg = params[:-2]
+    loc = params[-2]
+    scale = params[-1]
+
+    # Get sane start and end points of distribution
+    start = dist.ppf(0.01, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.01, loc=loc, scale=scale)
+    end = dist.ppf(0.99, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.99, loc=loc, scale=scale)
+
+    # Build PDF and turn into pandas Series
+    x = np.linspace(start, end, size)
+    y = dist.pdf(x, loc=loc, scale=scale, *arg)
+    pdf = pd.Series(y, x)
+
+    return pdf
+
+# Load data from statsmodels datasets
+def find_best_fitted_dist_for_features(feature_df:pd.DataFrame, _bins):
+
+    distributions_dict = {}
+    size = feature_df.shape[0]
+    for feature_col in list(feature_df.columns[5:6]):
+        data = feature_df[feature_col]
+        best_dist = best_fit_distribution(data, bins=100)
+        b = best_dist[0]
+        print(b)
+        pdf = make_pdf(b[0], b[1], size=size)
+        distributions_dict[feature_col] = [b, pdf]
+    return distributions_dict
