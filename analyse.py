3441a445354c6e9d0cc521d3de8f02bd42ba340c
DimPap99 <dimtrispap1999@gmail.com>
2023-10-28 03:36:04 +0200

    some cleanups and data organisation

--- analyse.py
+++ analyse.py
@@ -30,7 +30,7 @@ def get_num_of_nodes_per_cat(Timestep, graph:Graph, subgraphs_list:list, verbose
             f.write(res_str)
     return results
 
-def subgraph_list(graph:Graph, subgraph_list):
+def get_features_from_subgraph(graph:Graph, subgraph_list):
     result_feat_dict= {}
     subg_result_dict = {}
     for subg in subgraph_list:
@@ -53,4 +53,32 @@ def subgraph_list(graph:Graph, subgraph_list):
                     result_feat_dict[key][value] = 1
                 else:
                     result_feat_dict[key][value] +=1
+    #return total feature counts for all nodes and feature counts per subgraph
     return result_feat_dict, subg_result_dict
+
+
+def get_features_per_group(feature_dict):
+    features_per_cat_no_counts = {}
+
+    for key, value in feature_dict.items():
+        for feat, vals in value.items():
+            if feat not in features_per_cat_no_counts:
+                features_per_cat_no_counts[feat] = set()
+            for v,_ in vals.items():
+                features_per_cat_no_counts[feat].add(v)
+    return features_per_cat_no_counts
+
+def find_differences_between_groups(attr_set_1, attr_set_2):
+    features_in_1 = {}
+    features_in_2 = {}
+    for k, v in attr_set_1.items():
+        print("Features that exist only in Quality Group 1 and not in Quality Group 2")
+        d = v.difference(attr_set_2[k])
+        print(k, d)
+        features_in_1[k] = d
+        print("Features that exist only in Quality Group 2 and not in Quality Group 1")
+        d2 = attr_set_2[k].difference(v)
+        print(k, d2)
+        features_in_2[k] = d2
+        print("\n")
+    return features_in_1, features_in_2
--- main.py
+++ main.py
@@ -10,53 +10,55 @@ from helpers import bfs, get_class_pairs, choose_node_based_on_neighbors
 import numpy as np
 import networkx as nx
 from Graph import Graph, Subgraph
-from GraphMining import beam_search
+from GraphMining import mine_graphs
+import sys
+from analyse import get_num_of_nodes_per_cat
+from multiprocessing import Pool, Manager
+from helpers import export_csv, pickle_data
 
-THRESHOLD = 0.2
-TIMESTEPS =[13]#, 42, 35, 32, 29, 22, 9 ,20]
+MINED_GRAPHS = "Mined_Subgraphs"
+DATA_DIRECTORY = "pickled_objs"
+NUM_OF_PROCESSES = 4
+graph_dict:dict = load_pickled_data(os.path.join(DATA_DIRECTORY, "working_graphs_dict3.pkl"))
+AVAILABLE_TIMESTEPS = list(graph_dict.keys())
+print(AVAILABLE_TIMESTEPS)
+THRESHOLD = 0.25
+#TIMESTEPS = [9, 13, 15, 16, 17, 20, 21, 22, 24, 25, 26, 29, 31, 32, 35, 38, 40, 41, 42] #---> those timesteps contain most illicit nodes
+MIN_SUBGRAPH_SIZE = 12
+MAX_SIZE = None
+MINE_UNEXPLORED = True
+def mine_timesteps_in_parallel(Timestep):
 
-DATASET_BASE = "dataset"
-PICKLED_TIMESTEPS = os.path.join("exports", "split_on_timestep.pkl")
+    print(f"Starting job for Timestep {Timestep}...")
+    SAVE_RESULTS_DIR = os.path.join("Results", f"Timestep_{Timestep}.txt")
 
-df:pd.DataFrame = load_pickled_data("discretized_df.pkl")
-
-main_graph:Graph = load_pickled_data("working_graph.pkl")
-
-initial_beamsearch_candidates = main_graph.get_step_initial_nodes()
-
-beam_candidates = []
-
-for candidate in initial_beamsearch_candidates:
-    s = Subgraph()
-    s.closed.add(candidate)
-    s.quality = s.calculate_quality(main_graph, THRESHOLD)
-    if s.quality > 0:
-        candidate_children:set = main_graph.nodes[candidate].children
-        s.open = s.open.union(candidate_children)
-        beam_candidates.append(s)
-interesting_graphs, rejected_candidates = beam_search(graph=main_graph, threshold=THRESHOLD, beam_width=4, initial_candidates=beam_candidates)
-beam_candidates = []
-
-while len(rejected_candidates) != 0:
-    for candidate in rejected_candidates:
-        s = Subgraph()
-        s.closed.add(candidate)
-        s.quality = s.calculate_quality(main_graph, THRESHOLD)
-        if s.quality > 0:
-            candidate_children:set = main_graph.nodes[candidate].children
-            s.open = s.open.union(candidate_children)
-            beam_candidates.append(s)
+    main_graph:Graph = graph_dict[Timestep]
+    interesting_subgraphs = mine_graphs(main_graph, THRESHOLD, 4, verbose=False, min_size=MIN_SUBGRAPH_SIZE, mine_unexplored=MINE_UNEXPLORED, max_size=None)
+    print(f"Timestep-{Timestep}. Finished mining: {len(interesting_subgraphs)} subgraphs with min_size: {MIN_SUBGRAPH_SIZE}")
     
-    subgraph_results, new_rejected_candidates = beam_search(graph=main_graph, threshold=THRESHOLD, beam_width=4, initial_candidates=beam_candidates)
-    interesting_graphs = interesting_graphs + subgraph_results
-    rejected_candidates = new_rejected_candidates.difference(rejected_candidates)
-    beam_candidates = []
+   
+    res = get_num_of_nodes_per_cat(Timestep, main_graph, interesting_subgraphs, verbose=False, save_results_path=SAVE_RESULTS_DIR)
+    
+    print(f"Finished job for Timestep {Timestep}...")
+    return res, interesting_subgraphs
 
-sz_dict = {}
-for subgraph in interesting_graphs:
-    l = len(subgraph.closed)
-    if l not in sz_dict:
-        sz_dict[l] = 1
-    else:
-        sz_dict[l]+=1
-print(sz_dict)
+if __name__ == '__main__':
+    with Pool(NUM_OF_PROCESSES) as p:
+        
+        results = p.map(mine_timesteps_in_parallel, AVAILABLE_TIMESTEPS)
+
+    csv_data = []
+    result_subgraphs = []
+
+    _headers =["TimeStep", "Sid", "Quality", "Illicit", "Licit", "Unknown", "size"]
+
+    for result in results:
+        for l in result[0]:
+            csv_data.extend([l])
+        for s in result[1]:
+            result_subgraphs.append(s)
+            
+    csv_path = os.path.join(MINED_GRAPHS, f"Results_sz__min{MIN_SUBGRAPH_SIZE}_max_{MAX_SIZE}_thresh_{str(THRESHOLD).replace('.','_')}_{MINE_UNEXPLORED}.csv")
+    sub_pkl_path = os.path.join(DATA_DIRECTORY, f"Subgraphs__min{MIN_SUBGRAPH_SIZE}_max_{MAX_SIZE}_thresh_{str(THRESHOLD).replace('.','_')}_{MINE_UNEXPLORED}.pkl")
+    export_csv(file_path=csv_path,  data=csv_data, isDataframe=False, headers=_headers)
+    pickle_data(file_path=sub_pkl_path,  obj=result_subgraphs, verbose=True)
